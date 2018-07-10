f935c6bff40fe3c27471049a4def3174698dd65d
Unknown <clayjlindstrom@gmail.com>
2018-07-09 21:36:50 -0600

    Got the main character to move around, and starting to work on making the houses and characters clickable.

---
+++ Assets/Scripts/NamelessProtagonistScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NamelessProtagonistScript : CharacterParent {
+
+	// Use this for initialization
+	void Start () {
+		rectTransform = GetComponent<RectTransform>();
+		
+		speed = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		float y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		rectTransform.transform.Translate(x, y, 0f);
+	}
+}
